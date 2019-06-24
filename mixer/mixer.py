1fd96a4d5a8b35d10bac895a0feef5a6bec70e0f
Adam Cathersides <adamcathersides@gmail.com>
2019-06-25 00:50:00 +0100

    Now records state

--- mixer/mixer.py
+++ mixer/mixer.py
@@ -3,36 +3,39 @@
 import functools
 import netifaces
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-
-from mixer.db import get_db
+import json
+# from mixer.db import get_db
 
 bp = Blueprint('mixer', __name__, url_prefix='/mixer')
 
 interface = 'wlp2s0'
 ip_addr = netifaces.ifaddresses(interface)[2][0]['addr']
 
+
 @bp.route('/<mix>', methods=('GET', 'POST'))
 def mixer(mix):
 
-        if error is None:
-            db.execute(
-                'INSERT INTO aux1 (channel1) VALUES (?)',
-                (channel1)
-            )
-            db.execute(
-                'INSERT INTO aux1 (channel2) VALUES (?)',
-                (channel2)
-            )
-            db.execute(
-                'INSERT INTO aux1 (channel3) VALUES (?)',
-                (channel3)
-            )
-            db.execute(
-                'INSERT INTO aux1 (channel4) VALUES (?)',
-                (channel4)
-            )
-            db.commit()
-            return 'done'
+    with open('mixer/channelmap.json', 'r') as mapfile:
+        channel_map = json.load(mapfile)
+        # if error is None:
+            # db.execute(
+                # 'INSERT INTO aux1 (channel1) VALUES (?)',
+                # (channel1)
+            # )
+            # db.execute(
+                # 'INSERT INTO aux1 (channel2) VALUES (?)',
+                # (channel2)
+            # )
+            # db.execute(
+                # 'INSERT INTO aux1 (channel3) VALUES (?)',
+                # (channel3)
+            # )
+            # db.execute(
+                # 'INSERT INTO aux1 (channel4) VALUES (?)',
+                # (channel4)
+            # )
+            # db.commit()
+            # return 'done'
 
-    return render_template('mixer/mixer.html', ip_addr=ip_addr, mix=mix)
+    return render_template('mixer/mixer.html', ip_addr=ip_addr, mix=mix, channel_map=channel_map)
 
--- mixer/rest.py
+++ mixer/rest.py
@@ -1,15 +1,14 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 import send_cc
+import json
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-# aux_map[aux][channel]
 
-
-def _createChannelMmap():
-    """ Create the map of mix--> channel--> midi cc number"""
+def _createChannelMap():
 
     mix_map = {}
     for mix in range(1,5):
@@ -33,9 +32,19 @@ class Mixer(Resource):
     def __init__(self):
         self.midi = send_cc.midi(3)
 
+        if os.path.isfile('./channelmap.json'):
+            with open('channelmap.json', 'r') as mapfile:
+                self.channelMap = json.load(mapfile)
+        else:
+            self.channelMap = _createChannelMap()
+
     def post(self, aux, channel, value):
 
-        cc = channelMap[f'aux{aux}'][f'channel{channel}']['cc']
+        cc = self.channelMap[f'aux{aux}'][f'channel{channel}']['cc']
+        self.channelMap[f'aux{aux}'][f'channel{channel}']['value'] = value
+
+        with open('channelmap.json', 'w') as mapfile:
+            json.dump(self.channelMap, mapfile)
 
         self.midi.cc_tx(cc, value)
         return {f'aux{aux}':{channel:value}}
@@ -52,5 +61,4 @@ class Mixer(Resource):
 api.add_resource(Mixer, '/mixer/aux<int:aux>/<int:channel>/<int:value>', endpoint = 'mixer')
 
 if __name__ == '__main__':
-    channelMap = _createChannelMmap()
     app.run(debug=True, host='0.0.0.0', port=5001)
